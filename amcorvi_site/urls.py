d94a5231450a678f862d05b17ed60b8eafb87bfd
AMCorvi <amcorvi@icloud.com>
2017-01-23 05:22:54 -0500

    Added Projects, Blog and Blog_Tags models

--- amcorvi_site/urls.py
+++ amcorvi_site/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from mainsite import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', views.index),
+    # url(r'^blog/$', include('blog.views')
 ]
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Tags(models.Model):
+   tag_name = models.CharField(max_length=15)
+
+class Blog(models.Model):
+   post_title = models.CharField(max_length=50)
+   post_content = models.TextField()
+   date_create = models.DateTimeField('data published')
+   img_url = models.CharField(max_length=300)
+   tags = models.ManyToManyField(Tags)
+
