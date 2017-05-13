0f76d77b7e4d1571535f742d38d12a401ebcc935
AMCorvi <amcorvi@icloud.com>
2017-05-13 07:04:44 -0400

    - Minor changes to file structures for styles - correction to subdomain configurations

--- amcorvi_site/settings.py
+++ amcorvi_site/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['localhost',
                  'corvi.dev',
                  'blog.corvi.dev',
                  'projects.corvi.dev',
-                 ]
+                ]
 
 
 # Application definition
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE =  'America/New_York'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
