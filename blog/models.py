b3739bb608c1e3ec060437f4be3ed3e6ddeff703
A M Corvi <amcorvi@icloud.com>
2017-07-28 07:58:57 -0400

    😡

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Blog(models.Model):
    author = models.CharField(max_length=20, blank=True)
    post_title = models.CharField(max_length=50)
    post_content = models.TextField()
-   date_created = models.DateTimeField('date published')
+   date_created = models.DateTimeField(auto_now_add=True)
    img_url = models.URLField(max_length=300, blank=True)
    publish = models.BooleanField(default=True)
    tags = models.ManyToManyField(Tag)
